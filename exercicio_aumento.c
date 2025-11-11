e872db014c937c482f3b0882d589af05b6a4be1f
lucas07343 <lucassantosnunes07@gmail.com>
2025-11-11 11:54:48 -0300

    correção do exercicio aumento

--- exercicio_aumento.c
+++ exercicio_aumento.c
@@ -9,25 +9,20 @@ int main()
     scanf("%lf", &salario);
 
     if (salario <= 1000) {
-        aumento = salario / 100 * 20;
-        salarioNovo = salario + aumento;
         porcentagem = 20;
     }
     else if (salario <= 3000) {
-        aumento = salario / 100 * 15;
-        salarioNovo = salario + aumento;
         porcentagem = 15;
     }
     else if (salario <= 8000) {
-        aumento = salario / 100 * 10;
-        salarioNovo = salario + aumento;
         porcentagem = 10;
     }
     else {
-        aumento = salario / 100 * 5;
-        salarioNovo = salario + aumento;
         porcentagem = 5;
     }
+
+    aumento = salario / 100 * porcentagem;
+    salarioNovo = salario + aumento;
 
     printf("\nNovo salario = R$ %.2lf\n", salarioNovo);
     printf("Aumento = R$ %.2lf\n", aumento);
